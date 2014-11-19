3a51433f0b86c80bd656d462a5ec51116c344564
Filippo Squillace <squillace.filippo@gmail.com>
2014-11-20 00:01:11 +0100

    Add tests for util.sh #12

--- lib/util.sh
+++ lib/util.sh
@@ -17,7 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-echoerr() { echo "$@" 1>&2; }
+function echoerr(){
+    echo "$@" 1>&2
+}
 function die(){
 # $@: msg (mandatory) - str: Message to print
     error $@
