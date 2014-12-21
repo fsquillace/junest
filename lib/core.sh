12507bfaede30ce5f80ba38d554ec059bdbb860d
Filippo Squillace <squillace.filippo@gmail.com>
2014-12-21 21:34:24 +0100

    Fix issue #38

--- lib/core.sh
+++ lib/core.sh
@@ -204,6 +204,8 @@ function delete_juju(){
             die "Try to delete juju using root permissions"
         fi
     fi
+    # the CA directories are read only and can be deleted only by changing the mod
+    chmod -R +w ${JUJU_HOME}/etc/ca-certificates
     if rm -rf ${JUJU_HOME}/*
     then
         info "JuJu deleted in ${JUJU_HOME}"
