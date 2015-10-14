2cfb81daaa07d97834cfeeb6b779c913865f36d8
Filippo Squillace <squillace.filippo@gmail.com>
2015-10-14 22:34:25 +0000

    Issue #113: Fix fallback for proot command

--- tests/test_core.sh
+++ tests/test_core.sh
@@ -253,10 +253,9 @@ function test_run_env_as_user(){
     assertEquals "$output" "/usr/bin/mkdir"
     assertTrue "[ -e $JUNEST_HOME/newdir2 ]"
 
-    SH=("/usr/bin/mkdir" "-v" "/newdir")
-    local output=$(run_env_as_user "-k 3.10" | awk -F: '{print $1}')
-    assertEquals "$output" "/usr/bin/mkdir"
-    assertTrue "[ -e $JUNEST_HOME/newdir ]"
+    SH=("/usr/bin/echo")
+    local output=$(run_env_as_user "-k 3.10")
+    assertEquals "-c :" "$output"
 }
 
 function test_run_env_as_proot_mtab(){
@@ -329,7 +328,10 @@ function test_run_proot_seccomp(){
     }
     PROOT_COMPAT=envv
     local output=$(proot_cmd 2> /dev/null | grep "^PROOT_NO_SECCOMP")
-    assertEquals "PROOT_NO_SECCOMP=1" "$output"
+    # The variable PROOT_NO_SECCOMP will be produced
+    # twice due to the fallback mechanism
+    assertEquals "PROOT_NO_SECCOMP=1
+PROOT_NO_SECCOMP=1" "$output"
 }
 
 function test_run_env_as_fakeroot(){
