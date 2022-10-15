56761f6e630f0dd81eaf516a8b698bac11156c1a
Filippo Squillace <squillace.filippo@gmail.com>
2022-10-15 12:20:44 +0200

    Change url

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,8 @@ IMG_PATH=$1
 
 set -ux
 
-MAX_OLD_IMAGES=30
+MAX_OLD_IMAGES=5
+ENDPOINT="https://gateway.storjshare.io"
 
 # ARCH can be one of: x86, x86_64, arm
 HOST_ARCH=$(uname -m)
@@ -32,16 +33,13 @@ then
     # The put is done via a temporary filename in order to prevent outage on the
     # production file for a longer period of time.
     img_name=$(basename "${IMG_PATH}")
-    cp "${IMG_PATH}" "${IMG_PATH}".temp
-    aws s3 cp "${IMG_PATH}".temp s3://junest-repo/junest/
-    aws s3 mv s3://junest-repo/junest/"$img_name".temp s3://junest-repo/junest/"$img_name"
+    aws s3 --endpoint-url="$ENDPOINT" cp "${IMG_PATH}" s3://junest-repo/junest/
 
     DATE=$(date +'%Y-%m-%d-%H-%M-%S')
-
-    aws s3 cp "s3://junest-repo/junest/$img_name" "s3://junest-repo/junest/${img_name}.${DATE}"
+    aws s3 --endpoint-url="$ENDPOINT" cp "${IMG_PATH}" "s3://junest-repo/junest/${img_name}.${DATE}"
 
     # Cleanup old images
-    aws s3 ls s3://junest-repo/junest/junest-${ARCH}.tar.gz. | awk '{print $4}' | head -n -${MAX_OLD_IMAGES} | xargs -I {} aws s3 rm "s3://junest-repo/junest/{}"
+    aws s3 --endpoint-url="$ENDPOINT" ls s3://junest-repo/junest/junest-${ARCH}.tar.gz. | awk '{print $4}' | head -n -${MAX_OLD_IMAGES} | xargs -I {} aws s3 --endpoint-url="$ENDPOINT" rm "s3://junest-repo/junest/{}"
 
     # Test the newly deployed image can be downloaded correctly
     junest setup
