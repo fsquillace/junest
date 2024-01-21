13be0216119cd1969e804a2e49e1f3f5063c20f1
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-01-21 04:40:06 +0100

    Update junest-updater.sh

--- junest-updater.sh
+++ junest-updater.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # DOWNLOAD THE ARCHIVE
-wget https://github.com/ivan-hc/junest/releases/download/continuous/junest-x86_64.tar.gz
+#wget https://github.com/ivan-hc/junest/releases/download/continuous/junest-x86_64.tar.gz
 
 # SET APPDIR AS A TEMPORARY $HOME DIRECTORY, THIS WILL DO ALL WORK INTO THE APPDIR
 HOME="$(dirname "$(readlink -f $0)")" 
 
 # DOWNLOAD AND INSTALL JUNEST (DON'T TOUCH THIS)
 git clone https://github.com/fsquillace/junest.git ~/.local/share/junest
-./.local/share/junest/bin/junest setup -i junest-x86_64.tar.gz
+./.local/share/junest/bin/junest setup #-i junest-x86_64.tar.gz
 
 # BYPASS SIGNATURE CHECK LEVEL
-sed -i 's/#SigLevel/SigLevel/g' ./.junest/etc/pacman.conf
-sed -i 's/Required DatabaseOptional/Never/g' ./.junest/etc/pacman.conf
+#sed -i 's/#SigLevel/SigLevel/g' ./.junest/etc/pacman.conf
+#sed -i 's/Required DatabaseOptional/Never/g' ./.junest/etc/pacman.conf
 
 # UPDATE ARCH LINUX IN JUNEST
 ./.local/share/junest/bin/junest -- sudo pacman -Syy
